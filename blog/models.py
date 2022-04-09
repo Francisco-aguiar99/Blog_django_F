be45862add629c2f1e09490cedee34c03c1ed4db
francisco Aguiar <francisco@gmail.com>
2022-04-09 13:51:09 -0400

    outras

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from turtle import update
 from venv import create
 from django.contrib.auth.models import User 
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -12,7 +13,14 @@ class Post(models.Model):
     create = models.DateTimeField(auto_now_add=True) # vai guarda data e hora automatico
     updated = models.DateTimeField(auto_now=True) # vai salva as modifições 
 
+    class Meta:
+        ordering = ("-create",) # ordena os post em oreder que foi criado
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+    
 
 
